383ddc55cf5dafb8408d57252ebe010852fe4d91
Yuichi Tateno <hotchpotch@gmail.com>
2009-09-18 16:01:08 +0900

    init by cutagem

---
+++ lib/chrome/extension/starter.rb
@@ -0,0 +1,4 @@
+
+class Chrome::Extension::Starter
+
+end
---
+++ test/chrome-extension-starter_test.rb
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+require "test/unit"
+class Chrome::Extension::StarterTest < Test::Unit::TestCase
+end
